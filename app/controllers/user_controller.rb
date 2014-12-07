94d645043180128195b85d2c0db44e43c6d52905
Joel Klabo <joelklabo@gmail.com>
2014-12-06 17:26:19 -0800

    adding authentication

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,19 @@ class UserController < ApplicationController
   def index
     render json: User.all
   end
-  
+
+  def authenticate
+
+    @user = User.find_by(email: params[:email]) 
+    if @user && @user.password == params[:password]
+      render json: give_token, status: 200
+    else
+      render json: {'error' => 'failed to authenticate'}, status: 403
+    end
+    
+  end
+
+
   def create
 
     @user = User.new(user_params)
@@ -17,10 +29,22 @@ class UserController < ApplicationController
 
   end
 
+  
+  def give_token
+
+    {'token' => '2348h23kh324hk243jkhk423h2'}
+
+  end
+
+
   private
 
+
   def user_params
+
     params.require(:user).permit(:name, :email, :password_hash)
+
   end
+
 
 end
