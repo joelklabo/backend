4d0aaa07fc00ffc57dba89093b63b855b742462b
Joel Klabo <joelklabo@gmail.com>
2014-12-09 09:22:08 -0800

    not sure

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,7 +18,7 @@ class UserController < ApplicationController
 
   def create
 
-    @user = User.new(user_params)
+   @user = User.new(user_params)
     @user.password = params[:password]
 
     if @user.save
